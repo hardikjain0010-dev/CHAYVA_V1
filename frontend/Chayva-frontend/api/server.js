a1bb582daa48a640b825e85eeb7f0019669cb043
hardikjain0010-dev <hardikjain0010@gmail.com>
2026-08-08 23:43:51 +0530

    fix: remove problematic api/server.js file that was causing ES module require error

--- frontend/Chayva-frontend/api/server.js
+++
@@ -1,7 +0,0 @@
-// Vercel serverless function for TanStack Start SSR
-const { createServer } = require('../dist/server/server.js');
-
-module.exports = async (req, res) => {
-  const server = createServer();
-  await server(req, res);
-};
